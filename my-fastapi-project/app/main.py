2691bbd935642c2cd50f6d51dcabbe8dd9993b24
Shch2295404 <den2295404@gmail.com>
2025-01-23 09:49:54 +0500

    Добавил новые файлы и изменения

---
+++ my-fastapi-project/app/main.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI, Query, Depends
+from typing import Optional
+from datetime import date
+from pydantic import BaseModel
+
+from app.bookings.router import router as router_bookings
+
+
+app = FastAPI()
+
+app.include_router(router_bookings)
+
+class HotelsSearchArgs:
+    def __init__(
+        self,
+        location: str,
+        date_from: date,
+        date_to: date,
+        stars: Optional[int] = None,
+        has_spa: Optional[bool] = None,
+    ):
+        self.location = location
+        self.date_from = date_from
+        self.date_to = date_to
+        self.stars = stars
+        self.has_spa = has_spa
+
+
+class SHotel(BaseModel):
+    address: str
+    name: str
+    stars: int
+
+
+@app.get("/hotels")
+def get_hotel(
+    search_args: HotelsSearchArgs = Depends(),
+):
+    return search_args
+
+
+class SBooking(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
+
+
+@app.post("/bookings")
+def add_booking(booking: SBooking):
+    pass
