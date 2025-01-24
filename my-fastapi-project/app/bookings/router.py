2c6b8bc010cdb68880a456f552eec14b85f6e787
Shch2295404 <den2295404@gmail.com>
2025-01-24 21:02:35 +0500

    The development stage is Lesson 1.7. Authorization and authentication. Step 9 (Video 6)

--- my-fastapi-project/app/bookings/router.py
+++ my-fastapi-project/app/bookings/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 
 from app.bookings.dao import BookingDAO
 from app.bookings.schemas import SBooking
@@ -10,5 +10,8 @@ router = APIRouter(
 )
 
 @router.get("")
-async def get_bookings() -> list[SBooking]:
-    return await BookingDAO.find_all()
+async def get_bookings(request: Request): # -> list[SBooking]:
+    print(request.cookies)
+    print(request.url)
+    print(request.client)
+    # return await BookingDAO.find_all()
--- my-fastapi-project/app/config.py
+++ my-fastapi-project/app/config.py
@@ -14,9 +14,13 @@ class Settings(BaseSettings):
     def DATABASE_URL(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
+    SECRET_KEY: str
+    ALGORITHM: str
+
     # Со 2 версии Pydantic, class Config был заменен на атрибут model_config
     # class Config:
     #     env_file = ".env"
+
     model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings()
