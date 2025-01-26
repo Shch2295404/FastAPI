31f5640bd61803fa313ed56ce7846e1cb82265c0
Shch2295404 <den2295404@gmail.com>
2025-01-26 18:02:51 +0500

    Lesson 1.7. Step 14 (Video 8)

--- my-fastapi-project/app/users/dependesies.py
+++ my-fastapi-project/app/users/dependesies.py
@@ -1,28 +1,34 @@
 from datetime import UTC, datetime
-from fastapi import Depends, HTTPException, Request, status
+from fastapi import Depends, Request
 from jose import jwt, JWTError
 
 from app.config import settings
+from app.exceptions import (
+    IncorrectTokenFormatException, 
+    TokenAbsentException, 
+    TokenExpiredException, 
+    UserIsNotPresentException
+)
 from app.users.dao import UsersDAO
 
 def get_token(request: Request):
     token = request.cookies.get("booking_access_token")
     if not token:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="No token in cookies")
+        raise TokenAbsentException
     return token
 
 async def get_current_user(token: str = Depends(get_token)):
     try:
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
     except JWTError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+        raise IncorrectTokenFormatException
     expire: str = payload.get("exp")
     if (not expire) or (int(expire) < datetime.now(UTC).timestamp()):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token expired")
+        raise TokenExpiredException
     user_id: str = payload.get("sub")
     if not user_id:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="No user id in token")
+        raise UserIsNotPresentException
     user = await UsersDAO.find_by_id(int(user_id))
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
+        raise UserIsNotPresentException
     return user
