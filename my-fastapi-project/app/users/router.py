c9b70390e34bef39c330a8da263467aa381c8509
Shch2295404 <den2295404@gmail.com>
2025-01-26 17:08:00 +0500

    Lesson 1.7. Step 12 (Video 7)

--- my-fastapi-project/app/users/router.py
+++ my-fastapi-project/app/users/router.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, HTTPException, Response, status
+from fastapi import APIRouter, Depends, HTTPException, Response, status
 
 from app.users.schemas import SUserAuth
 from app.users.dao import UsersDAO
 from app.users.auth import authenticate_user, create_access_token, get_password_hash
+from app.users.dependesies import get_current_user
+from app.users.models import Users
 
 
 router = APIRouter(
@@ -12,6 +14,9 @@ router = APIRouter(
 
 @router.post("/register")
 async def register_user(user_data: SUserAuth):
+    
+    """ Регистрация пользователя. """
+
     existing_user = await UsersDAO.find_one_or_none(email=user_data.email)
     if existing_user:
         raise HTTPException(status_code=500)
@@ -21,9 +26,29 @@ async def register_user(user_data: SUserAuth):
 
 @router.post("/login")
 async def login_user(response: Response, user_data: SUserAuth):
+    
+    """ Аутентификация пользователя. """
+
     user = await authenticate_user(user_data.email, user_data.password)
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
     access_token = create_access_token({"sub": str(user.id)})
     response.set_cookie(key="booking_access_token", value=access_token, httponly=True)
     return access_token
+
+
+@router.post("/logout")
+async def logout_user(response: Response):
+    
+    """ Удаление куки с токеном при выходе пользователя. """
+
+    response.delete_cookie(key="booking_access_token")
+    return {"detail": "User logged out successfully"}
+
+
+@router.get("/me")
+async def read_users_me(current_user: dict = Depends(get_current_user)):
+    
+    """ Возвращает данные о текущем авторизованном пользователе. """
+    
+    return current_user
