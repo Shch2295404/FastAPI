a4bae5dcd0f074be29d4f93a9070dcb46a754703
Shch2295404 <den2295404@gmail.com>
2025-01-27 20:05:34 +0500

    Lesson 1.8. Step 2 (Video 1)

--- my-fastapi-project/app/bookings/dao.py
+++ my-fastapi-project/app/bookings/dao.py
@@ -4,3 +4,7 @@ from app.dao.base import BaseDAO
 
 class BookingDAO(BaseDAO):
     model = Bookings
+
+    @classmethod
+    async def add(cls):
+        pass
--- my-fastapi-project/app/bookings/router.py
+++ my-fastapi-project/app/bookings/router.py
@@ -12,5 +12,12 @@ router = APIRouter(
 )
 
 @router.get("")
-async def get_bookings(user: Users = Depends(get_current_user)): # -> list[SBooking]:
+async def get_bookings(user: Users = Depends(get_current_user)) -> list[SBooking]:
     return await BookingDAO.find_all(user_id=user.id)
+
+
+@router.post("")
+async def add_booking(
+    user: Users = Depends(get_current_user),
+):
+    return await BookingDAO.add()
