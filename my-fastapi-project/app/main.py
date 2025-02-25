44d3c5d9d4e493bcef1225d36f77ea6fab89c338
Shch2295404 <den2295404@gmail.com>
2025-02-25 18:06:18 +0500

    Lesson 1.9. Step 3 (Video 2)

--- my-fastapi-project/app/main.py
+++ my-fastapi-project/app/main.py
@@ -1,13 +1,11 @@
-from fastapi import FastAPI, Query, Depends
-from typing import Optional
-from datetime import date
-from pydantic import BaseModel
+from fastapi import FastAPI
 
 from app.users.router import router as router_users
 from app.bookings.router import router as router_bookings
 from app.hotels.router import router as router_hotels
 from app.hotels.rooms.router import router as router_rooms
 
+from app.pages.router import router as router_pages
 
 
 app = FastAPI()
@@ -17,30 +15,4 @@ app.include_router(router_hotels)
 app.include_router(router_rooms)
 app.include_router(router_bookings)
 
-# class HotelsSearchArgs:
-#     def __init__(
-#         self,
-#         location: str,
-#         date_from: date,
-#         date_to: date,
-#         stars: Optional[int] = None,
-#         has_spa: Optional[bool] = None,
-#     ):
-#         self.location = location
-#         self.date_from = date_from
-#         self.date_to = date_to
-#         self.stars = stars
-#         self.has_spa = has_spa
-
-
-# class SHotel(BaseModel):
-#     address: str
-#     name: str
-#     stars: int
-
-
-# @app.get("/hotels")
-# def get_hotel(
-#     search_args: HotelsSearchArgs = Depends(),
-# ):
-#     return search_args
+app.include_router(router_pages)
---
+++ my-fastapi-project/app/pages/router.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Request, Depends
+from fastapi.templating import Jinja2Templates
+
+from app.hotels.router import get_hotels_by_location_and_time
+
+router = APIRouter(
+    prefix="/pages",
+    tags=["Фронтенд"],
+)
+
+tempates = Jinja2Templates(directory="app/templates")
+
+@router.get("/hotels")
+async def get_hotels_pages(
+    request: Request,
+    hotels=Depends(get_hotels_by_location_and_time),
+    ):
+    return tempates.TemplateResponse(
+        name = "hotels.html", 
+        context = {"request": request, "hotels": hotels},
+        )
