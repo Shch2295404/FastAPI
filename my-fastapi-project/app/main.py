9da71ffe68b659d5cafe978482bf0a0d3fff838d
Shch2295404 <den2295404@gmail.com>
2025-02-25 19:43:13 +0500

    Lesson 1.9. Step 4 (Video 3)

--- my-fastapi-project/app/main.py
+++ my-fastapi-project/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from app.users.router import router as router_users
 from app.bookings.router import router as router_bookings
@@ -6,9 +7,12 @@ from app.hotels.router import router as router_hotels
 from app.hotels.rooms.router import router as router_rooms
 
 from app.pages.router import router as router_pages
+from app.images.router import router as router_images
 
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 app.include_router(router_users)
 app.include_router(router_hotels)
@@ -16,3 +20,4 @@ app.include_router(router_rooms)
 app.include_router(router_bookings)
 
 app.include_router(router_pages)
+app.include_router(router_images)
