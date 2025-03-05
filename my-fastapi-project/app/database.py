a6e06bfd645d26646a5c620a665771d213e8ed1b
Shch2295404 <den2295404@gmail.com>
2025-03-05 22:01:10 +0500

    Lesson 2.5. Step 4 (Video 2)

--- my-fastapi-project/app/database.py
+++ my-fastapi-project/app/database.py
@@ -1,10 +1,18 @@
+from sqlalchemy import NullPool
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 from app.config import settings
 
 
-engine = create_async_engine(settings.DATABASE_URL)
+if settings.MODE == "TEST":
+    DATABASE_URL = settings.TEST_DATABASE_URL
+    DATABASE_PARAM = {"poolclass": NullPool}
+else:
+    DATABASE_URL = settings.DATABASE_URL
+    DATABASE_PARAM = {}
+
+engine = create_async_engine(DATABASE_URL, **DATABASE_PARAM)
 
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
