486f5a47ff0b414e088813781f06fcebfe53ba82
Shch2295404 <den2295404@gmail.com>
2025-03-06 20:30:07 +0500

    Lesson 2.5. Step 10 (Video 6)

---
+++ my-fastapi-project/app/tests/unit_tests/test_users/test_dao.py
@@ -0,0 +1,20 @@
+import pytest
+from app.users.dao import UsersDAO
+
+
+@pytest.mark.parametrize("user_id,email,exist", [
+    (1, "test@test.com", True), 
+    (2, "artem@example.com", True),
+    (3, "nonexistent@example.com", False)
+    ])
+async def test_find_user_by_id(user_id, email, exist):
+    user = await UsersDAO.find_by_id(user_id)
+
+    if exist:
+        assert user
+        assert user.id == user_id
+        assert user.email == email
+    else:
+        assert not user
+
+
